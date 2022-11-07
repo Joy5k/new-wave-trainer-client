d65a0f718fb5bf2ccbfe565a43ff2c0ca1e83477
Joy <mmehedihasanjoyv@gmail.com>
2022-11-07 22:12:11 +0600

    added navbar and blog site

---
+++ src/pages/Home/Demo/Demo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo = () => {
+    return (
+        <div>
+          <h3>service demo</h3>  
+        </div>
+    );
+};
+<h3>service demo</h3>
+export default Demo;
---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Demo from '../Demo/Demo';
+const Home = () => {
+    return (
+        <div>
+            <Demo></Demo>
+        </div>
+    );
+};
+
+export default Home;
